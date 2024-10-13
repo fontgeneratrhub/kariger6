2ca2ae94ff723df27b0be7d9abddc64325a4c909
fontgeneratrhub <fontgeneratorh@gmail.com>
2024-10-13 12:14:30 +0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/src/main.jsx'
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    fs: {
+      strict: false
+    }
+  }
+});
